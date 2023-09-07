8db41f22b540a9474df273335e05564722e4ce61
Nicolas Chartiot <nicolaschartiot@gmail.com>
2023-09-07 18:34:51 -0300

    Create route to get a random image based on API key owner

--- web/src/app/api/picture/route.ts
+++ web/src/app/api/picture/route.ts
@@ -1,4 +1,5 @@
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { verifyKey } from '@unkey/api';
 import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
@@ -19,41 +20,32 @@ const getRandomImageName = (images: FileObject[]) => {
   return randomImage.name;
 };
 
-const isKeyValid = async (key: string) => {
-  const res = await fetch('https://api.unkey.dev/v1/keys/verify', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ key }),
-  });
-
-  const json = await res.json();
-  return json?.valid;
-};
-
 export async function GET(req: NextRequest) {
+  // Get key from query
   const key = req.nextUrl.searchParams.get('key');
   if (!key) return NextResponse.json({ error: 'No key provided' }, { status: 400 });
 
-  const validKey = await isKeyValid(key);
-  // if (!validKey) return NextResponse.json({ error: 'Invalid key' }, { status: 400 });
+  // Verify key
+  const { result: unkeyResult, error: unkeyError } = await verifyKey(key);
+  if (unkeyError) return NextResponse.json({ error: unkeyError.message }, { status: 500 });
+  if (!unkeyResult.valid) return NextResponse.json({ error: 'Invalid key' }, { status: 400 });
 
+  // Verify user
   const supabase = createRouteHandlerClient({ cookies });
-  const { data } = await supabase.from('users').select('id').eq('api_key', key).single();
-  if (!data) return NextResponse.json({ error: 'User not found' }, { status: 404 });
+  const userId = unkeyResult.ownerId;
 
-  const userId = data.id;
-
+  // Get images
   const { data: images } = await supabase.storage.from('images').list(userId, {
     sortBy: {
       column: 'created_at',
       order: 'desc',
     },
   });
-  if (!images || images.length === 0)
+  if (!images || images.length === 0) {
     return NextResponse.json({ error: 'No images found' }, { status: 404 });
+  }
 
+  // Get random image
   const randomImage = getRandomImageName(images);
   const { data: imageData } = await supabase.storage
     .from('images')
