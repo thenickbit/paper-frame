22e10993e37b98361d522a9392a6182168a65d04
Nicolas Chartiot <nicolaschartiot@gmail.com>
2023-09-10 18:43:25 -0300

    Add classNames to typography

--- web/src/components/ui/typography.tsx
+++ web/src/components/ui/typography.tsx
@@ -1,33 +1,53 @@
+import { cn } from '@/utils/cn';
+
 type TypographyProps = {
   children: React.ReactNode;
+  className?: string;
 };
 
-export function TypographyH1({ children }: TypographyProps) {
+export function TypographyH1({ children, className }: TypographyProps) {
   return (
-    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">{children}</h1>
+    <h1 className={cn('scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl', className)}>
+      {children}
+    </h1>
   );
 }
 
-export function TypographyH2({ children }: TypographyProps) {
+export function TypographyH2({ children, className }: TypographyProps) {
   return (
-    <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+    <h2
+      className={cn(
+        'scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0',
+        className
+      )}
+    >
       {children}
     </h2>
   );
 }
 
-export function TypographyH3({ children }: TypographyProps) {
-  return <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">{children}</h3>;
+export function TypographyH3({ children, className }: TypographyProps) {
+  return (
+    <h3 className={cn('scroll-m-20 text-2xl font-semibold tracking-tight', className)}>
+      {children}
+    </h3>
+  );
 }
 
-export function TypographyH4({ children }: TypographyProps) {
-  return <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">{children}</h4>;
+export function TypographyH4({ children, className }: TypographyProps) {
+  return (
+    <h4 className={cn('scroll-m-20 text-xl font-semibold tracking-tight', className)}>
+      {children}
+    </h4>
+  );
 }
 
-export function TypographyP({ children }: TypographyProps) {
-  return <p className="leading-7 [&:not(:first-child)]:mt-6">{children}</p>;
+export function TypographyP({ children, className }: TypographyProps) {
+  return <p className={cn('leading-7 [&:not(:first-child)]:mt-6', className)}>{children}</p>;
 }
 
-export function TypographyBlockquote({ children }: TypographyProps) {
-  return <blockquote className="mt-6 border-l-2 pl-6 italic">{children}</blockquote>;
+export function TypographyBlockquote({ children, className }: TypographyProps) {
+  return (
+    <blockquote className={cn('mt-6 border-l-2 pl-6 italic', className)}>{children}</blockquote>
+  );
 }
