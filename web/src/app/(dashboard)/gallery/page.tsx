e3183d258e0603cce2f24e206a438e8f362ec7f1
Nicolas Chartiot <nicolaschartiot@gmail.com>
2023-10-05 15:22:51 -0300

    Use standard layout and loading for gallery page

--- web/src/app/(dashboard)/gallery/page.tsx
+++ web/src/app/(dashboard)/gallery/page.tsx
@@ -1,21 +1,11 @@
 import type { Metadata } from 'next';
 
 import { ImageGallery } from '../_components/image-gallery';
-import { ImageUploader } from '../_components/image-uploader';
 
 export const metadata: Metadata = {
   title: 'Gallery - PaperFrame',
 };
 
 export default function Gallery() {
-  return (
-    <div className="flex w-full flex-col items-center gap-4">
-      <div className="flex w-full items-center justify-between gap-4 p-8">
-        <h1 className="flex-1 text-4xl font-extrabold tracking-tight lg:text-5xl">Gallery</h1>
-        <ImageUploader />
-      </div>
-      <hr className="w-full " />
-      <ImageGallery />
-    </div>
-  );
+  return <ImageGallery />;
 }
