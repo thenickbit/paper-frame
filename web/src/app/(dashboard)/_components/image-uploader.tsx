a6ef99e138d87baf95baa14872a8c9be61da8809
Nicolas Chartiot <nicolaschartiot@gmail.com>
2023-10-05 18:13:58 -0300

    Add dialog and dropzone with state to avoid hydration errors

--- web/src/app/(dashboard)/_components/image-uploader.tsx
+++ web/src/app/(dashboard)/_components/image-uploader.tsx
@@ -2,17 +2,26 @@
 
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { useRouter } from 'next/navigation';
-import { useRef, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
 import { v4 as uuidv4 } from 'uuid';
 
 import { Button } from '@/components/ui/button';
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Icons } from '@/components/ui/icons';
 
 export const ImageUploader = () => {
   const supabase = createClientComponentClient();
   const router = useRouter();
+  const [openDialog, setOpenDialog] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    console.log(acceptedFiles);
+    // Do something with the files
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   const uploadFiles = async (files: FileList | null) => {
     if (!files) return;
@@ -56,12 +65,33 @@ export const ImageUploader = () => {
       <Button
         className="ml-auto"
         variant="outline"
-        onClick={() => inputRef.current?.click()}
         disabled={loading}
+        onClick={() => setOpenDialog(true)}
       >
         {loading ? 'Uploading' : 'Upload'}
-        {loading ? <Icons.spinner className="animate-spin" /> : <Icons.upload />}
+        <span className="ml-2">
+          {loading ? <Icons.spinner className="animate-spin" /> : <Icons.upload />}
+        </span>
       </Button>
+
+      <Dialog open={openDialog} onOpenChange={(state) => setOpenDialog(state)}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Upload images</DialogTitle>
+          </DialogHeader>
+          <div
+            {...getRootProps()}
+            className="rounded-md border border-dashed border-ring bg-secondary p-6"
+          >
+            <input {...getInputProps()} />
+            {isDragActive ? (
+              <p>Drop the files here ...</p>
+            ) : (
+              <p>Drag &apos;n&apos; drop some files here, or click to select files</p>
+            )}
+          </div>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
